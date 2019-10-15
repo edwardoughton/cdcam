c38a7bf714b0aa6a3b4ab9bc2960685a4bef83c6
ej550 <edward.oughton@gmail.com>
2019-10-15 11:04:58 +0100

    Add setup.py and inits

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for digital_comms.
+
+    This file was generated with PyScaffold 2.5.7, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
+
+"""
+import os
+import shutil
+import sys
+import getpass
+import re
+import zipfile
+from datetime import date
+from setuptools import setup
+
+
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['six', 'pyscaffold>=3.0a0,<3.1a0'] + sphinx,
+          use_pyscaffold=True)
+
+
+if __name__ == "__main__":
+
+    setup_package()
